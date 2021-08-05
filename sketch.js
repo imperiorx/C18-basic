ed354c8b941242eef175ad6ae573128736365f8a
imperiorx <86550040+imperiorx@users.noreply.github.com>
2021-08-05 16:48:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var student1;
+var student2;
+
+function setup(){
+  createCanvas(400,400);
+  student1=new student("imperior",14,8)
+  student1.display()
+student2=new student("atharva",16,9)
+student2.display()
+}
